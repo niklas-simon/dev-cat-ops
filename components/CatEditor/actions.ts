338c46ae68be37970f735c895cac66e6f935b830
Niklas <niklaspein12@gmail.com>
2025-04-10 07:42:58 +0200

    implement classifi-cat-ion

--- components/CatEditor/actions.ts
+++ components/CatEditor/actions.ts
@@ -8,11 +8,47 @@ import { Cat } from "@prisma/client";
 import { update, remove, create } from "@/access/cat";
 import { EditorCat } from "@/components/CatEditor/CatEditor";
 
-export const onSave = async (
+interface Classification {
+    cat_probability: number;
+    is_cat: boolean;
+}
+
+export async function onSave(
     cat: (Omit<Omit<Cat, "bytes">, "id"> & { id?: string }) | null,
     newCat: EditorCat,
     isNew: boolean,
-) => {
+): Promise<{ [key: string]: string } | undefined> {
+    if (!process.env.CLASSIFI_CAT_ION_URL) {
+        return {
+            form: "environment variable CLASSIFI_CAT_ION_URL is unset but required",
+        };
+    }
+
+    if (newCat.bytes && newCat.filename) {
+        const imageForm = new FormData();
+
+        imageForm.append(
+            "file",
+            new Blob([new Uint8Array(Buffer.from(newCat.bytes, "base64"))], {
+                type: "image/" + path.extname(newCat.filename).substring(1),
+            }),
+        );
+
+        const classification = await fetch(
+            process.env.CLASSIFI_CAT_ION_URL + "/predict",
+            {
+                method: "POST",
+                body: imageForm,
+            },
+        ).then((res) => res.json() as Promise<Classification>);
+
+        if (!classification.is_cat) {
+            return {
+                picture: "Das Bild muss eine Katze enthalten",
+            };
+        }
+    }
+
     if (isNew) {
         if (!newCat.bytes || !newCat.filename) {
             return;
@@ -27,7 +63,7 @@ export const onSave = async (
     } else {
         await update(Object.assign(cat!, newCat) as Cat);
     }
-};
+}
 
 export const onDelete = async (id: string) => {
     await remove(id);
